53e2cbba79b3dcf8a474692cb8296fe26090ca11
Josef <panjunfeng@hotmail.com>
2022-03-08 16:54:25 +1000

    Create String_Ext_Subscript.swift
    Extensions to acess String directly by integer index, or integer Range
    eg. string[2], string[2..<5], string[2...5], string[...5], string[..<5], string[2...]

---
+++ String_Ext_Subscript.swift
@@ -0,0 +1,57 @@
+import Foundation
+// Extensions to acess String directly by integer index, or integer Range
+// eg. string[2], string[2..<5], string[2...5], string[...5], string[..<5], string[2...]
+// Safe even integers exceeding boundaries
+extension String{
+    subscript (ix: Int) -> Character {
+        get {
+            let index = self.index(self.startIndex, offsetBy: ix)
+            return self[index]
+        }
+        set {
+            let index = self.index(self.startIndex, offsetBy: ix)
+            self.replaceSubrange(index...index, with: String(newValue))
+        }
+        
+    }
+    subscript (r: Range<Int>) -> String{
+        get {
+            let lowerBound  = min(max(r.lowerBound, 0), self.count-1) // Keep starting idx between 0 and boundary
+            let upperBound  = max(min(r.upperBound, self.count-1), 0) // Keep ending idx between 0 and boundary
+            let startIndex = self.index(self.startIndex, offsetBy: lowerBound)
+            let endIndex = self.index(self.startIndex, offsetBy: upperBound)
+            return String(self[startIndex..<endIndex])
+        }
+    }
+    subscript (r: ClosedRange<Int>) -> String{
+        get {
+            let lowerBound  = min(max(r.lowerBound, 0), self.count-1) // Keep starting idx between 0 and boundary
+            let upperBound  = max(min(r.upperBound, self.count-1), 0) // Keep ending idx between 0 and boundary
+            let startIndex = self.index(self.startIndex, offsetBy: lowerBound)
+            let endIndex = self.index(self.startIndex, offsetBy: upperBound)
+            return String(self[startIndex...endIndex])
+        }
+    }
+    subscript (r: PartialRangeUpTo<Int>) -> String{
+        get {
+            let upperBound  = max(min(r.upperBound, self.count-1), 0) // Keep starting idx between 0 and boundary
+            let endIndex = self.index(self.startIndex, offsetBy: upperBound)
+            return String(self[startIndex..<endIndex])
+        }
+    }
+    subscript (r: PartialRangeThrough<Int>) -> String{
+        get {
+            let upperBound  = max(min(r.upperBound, self.count-1), 0) // Keep ending idx between 0 and boundary
+            let endIndex = self.index(self.startIndex, offsetBy: upperBound)
+            return String(self[startIndex...endIndex])
+        }
+    }
+    subscript (r: PartialRangeFrom<Int>) -> String{
+        get {
+            let lowerBound  = min(max(r.lowerBound, 0), self.count-1) // Keep starting idx between 0 and boundary
+            let startIndex = self.index(self.startIndex, offsetBy: lowerBound)
+            return String(self[startIndex...])
+        }
+    }
+}
+
